fa85f30102905d464b42d51f1bf3610d59db5c70
Leonardo <info.leonardofalchi@gmail.com>
2025-09-03 16:15:04 +0200

    esercizio 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function getInitials(arrayNames) {
+    //arrey vuoto per iniziali
+    const initials = [];
+    //ciclo per prendere iniziali di ogni nome
+    for (let i = 0; i < arrayNames.length; i++) {
+        initials.push(arrayNames [i][0]);
+    }
+    //restitusci arrey iniziali
+    return initials
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
